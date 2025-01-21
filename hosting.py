00322851a9c8f5fd538f2ad12e9802d7b8fe81cd
Ludv1k <157578117+Ludv1k@users.noreply.github.com>
2025-01-21 11:21:26 +0100

    Added HTML, CSS, Javascript files alongside a python flask file for hosting

---
+++ hosting.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import random
+
+# Create the Flask app
+app = Flask(__name__)
+
+# Route for the homepage
+@app.route('/')
+def root():
+    return render_template('index.html')  # Renders the 'index.html' template
+
+# # Route for the testing page
+# @app.route('/download')
+# def notroot():
+#     return render_template('download.html')  # Renders the 'testing.html' template
+
+
+# Run the Flask app
+if __name__ == '__main__':
+    # Set the app to be accessible on the network
+    app.run(host='0.0.0.0', port=8080, debug=True)
